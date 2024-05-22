7b8b6673d6ca2bd5c08ceeefbc8e29149dff32d9
Harshad Lande <77875603+Harshad1705@users.noreply.github.com>
2024-05-22 21:48:15 +0530

    changes web security

--- src/main/java/com/harrySpringBoot/expensetrackerapi/config/WebSecurityConfig.java
+++ src/main/java/com/harrySpringBoot/expensetrackerapi/config/WebSecurityConfig.java
@@ -1,20 +1,16 @@
-package com.harrySpringBoot.expensetrackerapi.config;
+  package com.harrySpringBoot.expensetrackerapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -26,38 +22,25 @@ import com.harrySpringBoot.expensetrackerapi.security.JwtRequestFilter;
 @Configuration
 public class WebSecurityConfig {
 
-    
     @Autowired
     private CustomUserDetailService userDetailsService;
-
-    @Bean
-    public JwtRequestFilter authenticationJwtTokenFilter() {
-        return new JwtRequestFilter();
-    }
-
 
     // made "/login"  and "/register" public 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.
-                csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/login", "/register").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                
-        http.addFilterBefore(authenticationJwtTokenFilter(),UsernamePasswordAuthenticationFilter.class);
+        
 
-        http.httpBasic();
-
-        return http.build();
+        return  http.csrf(csrf->csrf.disable())
+            .authorizeHttpRequests(auth->auth.requestMatchers("/login","/register").permitAll().anyRequest().authenticated())
+            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+            .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     // use for Inmemory user detail
     // if want to use -
-        // then uncomment this code as well as CustomUserDetailService
+    // then uncomment this code as well as CustomUserDetailService
     // @Bean
     // public UserDetailsService userDetailsService() {
     //     UserDetails user1 = User.withDefaultPasswordEncoder()
@@ -73,27 +56,34 @@ public class WebSecurityConfig {
 
     //     return new InMemoryUserDetailsManager(user1, user2);
     // }
-    
 
-    // use to initialize AuthenticationManager used in AuthController
-     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+
+    @Bean
+    public JwtRequestFilter authenticationJwtTokenFilter() {
+        return new JwtRequestFilter();
     }
 
-     @Bean
-     public PasswordEncoder passwordEncoder() {
-         return new BCryptPasswordEncoder();
-     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     // use for custom user detail using mysql
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
+           DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+           authProvider.setUserDetailsService(userDetailsService);
+           authProvider.setPasswordEncoder(passwordEncoder());
+           return authProvider;
+    }
+
+
+    // use to initialize AuthenticationManager used in AuthController
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 
